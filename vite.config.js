53ff6dfa671ace63d5062383187847e14d354223
pixellab01 <shreekant@pixellab.media>
2026-02-07 12:51:29 +0000

    fixed domain

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['myvalentinegift.store'],
+    allowedHosts: ['myvalentinegift.store', 'all'],
     proxy: {
       '/api': {
         target: 'http://localhost:3002',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
